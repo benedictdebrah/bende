d785729e28d76abce402021fc8b83e96acc8df1a
Benedict <95214706+benedictdebrah@users.noreply.github.com>
2024-05-20 19:35:55 +0000

    changes to the service

--- site.config.js
+++ site.config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   projects: [
     {
       name: `bende`,
-      href: "https://github.com/benedictdebrah/bende",
+      href: "https://bende.vercel.app",
     },
   ],
   // blog setting (required)
@@ -23,7 +23,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://github.com/benedictdebrah/bende",
+  link: "https://bende.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
